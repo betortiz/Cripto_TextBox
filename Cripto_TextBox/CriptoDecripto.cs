b07899aab28b8e9907c1cf64f1eb6d595a5e08fa
betortiz <alberto.ortiz.medeiros@gmail.com>
2022-10-16 20:56:30 -0400

    Organizando codigo

---
+++ Cripto_TextBox/CriptoDecripto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Cripto_TextBox
+{
+    internal class CriptoDecripto
+    {      
+        internal void Cripto(TextBox textDecripto)
+        {
+            TextBox textCripto = new TextBox();
+            textCripto.Text = "";
+            textCripto.Visible = true;
+
+            TextBox textChave = new TextBox();
+            textChave.Text = "";
+
+            TextBox texDecripto = new TextBox();
+            textDecripto.Text = "";
+
+            //string textCripto = "";
+            //string textDecripto = "";
+            //string textChave = "";
+            int chave = Convert.ToInt32(textChave.Text);
+
+            for (int i = 0; i < textCripto.Text.Length; i++)
+            {
+                int txtUsuario = (int)textCripto.Text[i];
+                int txtCifrado = txtUsuario + chave;
+                textDecripto.Text += char.ConvertFromUtf32(txtCifrado);
+                Clipboard.SetText(textDecripto.Text);
+            }
+        }
+
+        internal void Decripto()
+        {
+            string textCripto = "";
+            string textDecripto = "";
+            int chave = 0;
+
+            for (int i = 0; i < textCripto.Length; i++)
+            {
+                int txtUsuario = (int)textCripto[i];
+                int txtCifrado = txtUsuario - chave;
+                textDecripto += char.ConvertFromUtf32(txtCifrado);
+            }
+        }
+    }
+}
--- Cripto_TextBox/Form1.cs
+++ Cripto_TextBox/Form1.cs
@@ -7,23 +7,6 @@ namespace Cripto_TextBox
             InitializeComponent();
         }
 
-        public void message_validator()
-        {
-            DialogResult r5 = MessageBox.Show("Digite apenas números de 1 a 9!",
-                 "Mensagem de aviso", MessageBoxButtons.OK,
-                 MessageBoxIcon.Question,
-                 MessageBoxDefaultButton.Button1);
-
-        }
-
-        public void messageAlert()
-        {
-            DialogResult r5 = MessageBox.Show("Sua menssagem foi criptografada e copiada para a area de transferencia!!!",
-                        "Mensagem de aviso", MessageBoxButtons.OK,
-                        MessageBoxIcon.Question,
-                        MessageBoxDefaultButton.Button1);
-        }
-
         private void bt_cripto_Click(object sender, EventArgs e)
         {
             if (textChave.Text == string.Empty)
@@ -31,42 +14,31 @@ namespace Cripto_TextBox
                 textChave.Text = "1";
             }
 
-            int chave = Convert.ToInt32(textChave.Text);
+            Mensagem m1 = new Mensagem();
+            m1.Alerta();
 
             textDecripto.Text = String.Empty;
-            for (int i = 0; i < textCripto.Text.Length; i++)
-            {
-                int txtUsuario = (int)textCripto.Text[i];
-                int txtCifrado = txtUsuario + chave;
-                textDecripto.Text += char.ConvertFromUtf32(txtCifrado);
-                Clipboard.SetText(textDecripto.Text);
-            }
-            messageAlert();
+
+            CriptoDecripto cripto = new CriptoDecripto();
+            cripto.Cripto(textDecripto);
         }
 
         private void bt_decripto_Click(object sender, EventArgs e)
         {
-            // ultimo ajuste
             if (textChave.Text == string.Empty)
             {
                 textChave.Text = "1";
             }
 
-            int chave = Convert.ToInt32(textChave.Text);
-
-
-
             textDecripto.Text = String.Empty;
-            for (int i = 0; i < textCripto.Text.Length; i++)
-            {
-                int txtUsuario = (int)textCripto.Text[i];
-                int txtCifrado = txtUsuario - chave;
-                textDecripto.Text += char.ConvertFromUtf32(txtCifrado);
-            }
+
+            CriptoDecripto decripto = new CriptoDecripto();
+            decripto.Decripto();
         }
 
         private void textChave_KeyPress(object sender, KeyPressEventArgs e)
         {
+
             if (e.KeyChar == '0')
             {
                 e.Handled = true;
@@ -82,9 +54,8 @@ namespace Cripto_TextBox
 
         private void bt_limpar_Click(object sender, EventArgs e)
         {
-            textChave.Text = String.Empty;
-            textCripto.Text = String.Empty;
-            textDecripto.Text = String.Empty;
+            Mensagem limpar = new Mensagem();
+            limpar.Limpar();
         }
 
         private void bt_sair_Click(object sender, EventArgs e)
@@ -103,10 +74,12 @@ namespace Cripto_TextBox
 
         private void textChave_TextChanged_1(object sender, EventArgs e)
         {
+            Mensagem m2 = new Mensagem();
+
             if (textChave.Text.Length > 1)
             {
                 textChave.Text = String.Empty;
-                message_validator();
+                m2.mensagem_validador();
             }
         }
     }
